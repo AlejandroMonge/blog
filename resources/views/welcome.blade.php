321e0766e4eae0a3b513474739003e8236c0c633
AlejandroMonge <kmonge_rey@hotmail.com>
2020-02-11 16:27:53 -0600

    Title Changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mi Blog
                 </div>
 
                 <div class="links">
